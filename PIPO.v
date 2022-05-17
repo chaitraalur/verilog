9927b9e17187f03e7fc2c2773a269c52b7301bfd
chaitraalur <102359148+chaitraalur@users.noreply.github.com>
2022-05-17 11:18:40 +0530

    Add files via upload

--- PIPO.v
+++ PIPO.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    15:35:09 03/21/2022 
+// Create Date:    16:28:08 04/08/2022 
 // Design Name: 
 // Module Name:    PIPO 
 // Project Name: 
@@ -18,11 +18,10 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module PIPO(data_out,data_in,load,clk);
-input[15:0] data_in;
-input load,clk;
-output reg[15:0] data_out;
-always@(posedge clk)
-if(load)data_out<=data_in;
+module PIPO (data_out, data_in, load, clk);
+input [15:0] data_in;
+input load, clk;
+output reg [15:0] data_out;
+always @(posedge clk)
+  if (load) data_out <= data_in;
 endmodule
-
